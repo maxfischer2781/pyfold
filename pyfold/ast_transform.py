b7d9c98e08f868cc13c84b74991229562b5a1ff6
Max Fischer <maxfischer2781@gmail.com>
2021-07-01 18:27:45 +0200

    simplified AST transformation

--- pyfold/ast_transform.py
+++ pyfold/ast_transform.py
@@ -5,21 +5,14 @@ import ast
 assert sys.version_info[:2] == (3, 9), f"In {sys.version_info}, AST folds you!"
 
 
-class NameFolder(ast.NodeTransformer):
-    """
-    AST Transformer that case-folds names
-    """
-
-    def visit_Name(self, node: ast.Name) -> ast.Name:
-        node.id = node.id.casefold()
-        return node
-
-    def visit_Attribute(self, node: ast.Attribute) -> ast.Attribute:
-        node.attr = node.attr.casefold()
-        return node
-
-
 def fold(source: Union[str, ast.AST]) -> ast.AST:
     """Convenience helper to fold an AST or source code"""
     source_ast = ast.parse(source) if isinstance(source, str) else source
-    return NameFolder().visit(source_ast)
+    for node in ast.walk(source_ast):
+        if isinstance(node, ast.Name):
+            node.id = node.id.casefold()
+        elif isinstance(node, ast.Attribute):
+            node.attr = node.attr.casefold()
+        elif isinstance(node, (ast.ClassDef, ast.FunctionDef)):
+            node.name = node.name.casefold()
+    return source_ast
